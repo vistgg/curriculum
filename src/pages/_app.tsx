f1ed6bf30806d67a85e610943611eb91d038d9dd
Ivo <ivopr@proton.me>
2023-02-01 17:23:39 -0400

    feat: google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -49,6 +49,30 @@ function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 					</SessionProvider>
 				</Suspense>
 			</div>
+
+			{process.env.NODE_ENV === "production" ? (
+				// <!-- Google tag (gtag.js) -->
+				<>
+					<Script
+						strategy="lazyOnload"
+						src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+					/>
+
+					<Script
+						id="ga4"
+						strategy="lazyOnload"
+					>
+						{`
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+							gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+							page_path: window.location.pathname,
+							});
+						`}
+					</Script>
+				</>
+			) : null}
 		</div>
 	);
 }
